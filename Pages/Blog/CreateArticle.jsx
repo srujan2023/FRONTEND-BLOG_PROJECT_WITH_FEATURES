be7231df55dd5389d96730857ef269eec65426f7
Srujan <Srujan2004ab@gmail.com>
2025-08-21 22:22:53 +0530

    Create Article Page Updated

--- Pages/Blog/CreateArticle.jsx
+++ Pages/Blog/CreateArticle.jsx
@@ -1,27 +1,53 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const CreateArticle = () => {
     const [title,setTitle] = useState({})
-    const [body,setbody] = useState({})
+    const [body,setBody] = useState({})
+    const [price,setPrice] = useState({})
+
+    const Navigate = useNavigate()
+
+    const CreateFormSubmited = async(e) =>{
+      e.preventDefault()
+
+      console.log(title);
+      console.log(body);
+      console.log(price);
+      
+      const response = await fetch('http://localhost:5000/api/blog/articles',{
+        method:"POST",
+         headers: {
+                 'Content-Type': 'application/json',
+                 //Authorization:`Bearer ${token}`
+             },
+               body: JSON.stringify({title,body,price})
+            })
+
+            const data = await response.json();
+
+            console.log(data);
+           Navigate('/admin/blog') 
+    }
     
 
   return (
     <>     
     <h1>Create Article</h1>
-    <form onClick={CreateFormSubmited}>
+     <form onClick={CreateFormSubmited}>
         <label >Title</label>
-        <input  onChange={(e)=>settitle(e.target.value)} type="text"  placeholder='Enter Article Name' />
+        <input  onChange={(e)=>setTitle(e.target.value)} type="text"  placeholder='Enter Article Name' />
         <br />
         <br />
         <label>Body</label>
-        <textarea onChange={(e)=>setbody(e.target.value)} placeholder='Enter Article Body ' ></textarea>
+        <textarea onChange={(e)=>setBody(e.target.value)} placeholder='Enter Article Body ' ></textarea>
         <br /><br />
         <label>Price</label>
-        <input  onChange={(e)=>setprice(e.target.value)}type="number" placeholder='Enter Article Amount' />
+        <input  onChange={(e)=>setPrice(e.target.value)} type="number" placeholder='Enter Article Amount' />
         <br />
         <br />
         <button>Create Article</button>
-    </form>
+    </form> 
      </>
   )
 }
